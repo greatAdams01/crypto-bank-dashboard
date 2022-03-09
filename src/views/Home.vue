9d624816367dd93643e327a81ce8a1b20c070a4a
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2022-03-09 04:26:51 +0100

    Added dashboard elements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,50 @@
 <template>
   <div>
-    <div class="container text-center">
-      <h2 class="text-dark">Welcome to crypto bank</h2>
+    <div class="container">
+      <div class="dashboard">
+        <div class="page-heading">
+          <h5 class="page-title">{{ $route.name }}</h5>
+          <p>Edit Details</p>
+        </div>
+
+        <div class="user-data">
+          <div class="user-item">
+            <div class="content">
+              <div class="user-info">
+                <div class="info">
+                  <p class="label">Full name</p>
+                  <p class="info-text">Great Adams</p>
+                </div>
+              </div>
+              <div class="user-info">
+                <div class="info">
+                  <p class="label">Sex</p>
+                  <p class="info-text">Male</p>
+                </div>
+                <div class="info">
+                  <p class="label">Occupation</p>
+                  <p class="info-text">Investor</p>
+                </div>
+              </div>
+              <div class="user-info">
+                <div class="info">
+                  <p class="label">Phone</p>
+                  <p class="info-text">Phone</p>
+                </div>
+                <div class="info">
+                  <p class="label">Email</p>
+                  <p class="info-text">greatadams@gmail.com</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="user-item">
+            <div class="content">
+              <img src="/img/wp5293460.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
