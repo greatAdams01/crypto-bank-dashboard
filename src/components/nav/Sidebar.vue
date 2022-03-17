f3eec4e2b637bb79a0286ee994cd846f91829334
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2022-03-17 04:27:20 +0100

    Added Tailwind

--- src/components/nav/Sidebar.vue
+++ src/components/nav/Sidebar.vue
@@ -1,32 +1,11 @@
 <template>
-  <div class="dash-side">
-    <div class="sidebar">
-      <div class="user-img">
-        <img src="/img/wp5293460.jpg" alt="">
-      </div>
-      <div class="user-name">
-        <h3>Great Adams</h3>
-      </div>
-      <div class="navigations">
-        <ul class="navigate-list">
-          <li
-            v-for="route in routes"
-            :key="route.url"
-            class="navigate-item"
-          >
-            <router-link :to="route.url" class="navigate-link">
-              <img :src=route.img alt="">
-              {{ route.title }}
-            </router-link>
-          </li>
-        </ul>
-      </div>
-      <div class="log-out">
-        <div class="log-div">
-          <img src="/img/log-out.svg" alt="">
-          <p>Logout</p>
-        </div>
-      </div>
+  <div class="flex">
+    <div class="fixed top-0">
+      <i>A</i>
+      <i>B</i>
+      <i>C</i>
+      <i>D</i>
+      <i>E</i>
     </div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,8 @@
 <template>
   <div>
-    <div class="container">
-      <div class="dashboard">
-        <div class="page-heading">
-          <h5 class="page-title">{{ $route.name }}</h5>
-          <p>Edit Details</p>
-        </div>
-
-        <div class="user-data">
-          <div class="user-item">
-            <div class="content">
-              <div class="user-info">
-                <div class="info">
-                  <p class="label">Full name</p>
-                  <p class="info-text">Great Adams</p>
-                </div>
-              </div>
-              <div class="user-info">
-                <div class="info">
-                  <p class="label">Sex</p>
-                  <p class="info-text">Male</p>
-                </div>
-                <div class="info">
-                  <p class="label">Occupation</p>
-                  <p class="info-text">Investor</p>
-                </div>
-              </div>
-              <div class="user-info">
-                <div class="info">
-                  <p class="label">Phone</p>
-                  <p class="info-text">Phone</p>
-                </div>
-                <div class="info">
-                  <p class="label">Email</p>
-                  <p class="info-text">greatadams@gmail.com</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="user-item">
-            <div class="content">
-              <img src="/img/wp5293460.jpg" alt="">
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   </div>
 </template>
 
